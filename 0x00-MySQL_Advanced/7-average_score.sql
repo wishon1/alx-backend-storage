2d5e8ef8a9aae18f14a2cadfba61ee4fa839c97e
wishon1 <wisdomhonest1@gmail.com>
2024-05-10 08:26:37 -0400

    add task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,33 +1,30 @@
--- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser
--- that computes and store the average score for a student.
+-- Write a SQL script that creates a stored procedure 
+-- ComputeAverageScoreForUser that computes and store the average
+-- score for a student.
 -- Note: An average score can be a decimal
 -- Requirements:
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
+-- user_id, a users.id value (you can assume user_id is linked to an
+-- existing users)
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE num_corrections INT;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    -- Calculate total score for the user
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-    -- Count the number of corrections for the user
-    SELECT COUNT(*) INTO num_corrections
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Calculate average score
-    IF num_corrections > 0 THEN
-        UPDATE users
-        SET average_score = total_score / num_corrections
-        WHERE id = user_id;
-    END IF;
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
 END $$
-
 DELIMITER ;
