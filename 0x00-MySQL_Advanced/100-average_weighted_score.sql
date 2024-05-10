299c985f3ea5af002e03a16df897d873bf49cf9b
wishon1 <wisdomhonest1@gmail.com>
2024-05-10 17:37:53 -0400

    add task 100

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,46 @@
+-- Write a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUser that computes and store the average
+-- weighted score for a student.
+-- Requirements:
+-- Procedure ComputeAverageScoreForUser is taking 1 input:
+-- user_id, a users.id value (you can assume user_id is linked to an
+-- existing users)
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
+BEGIN
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
+
+    -- Calculate the total weighted score for the user
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    -- Calculate the total weight of projects for the user
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    -- Update the user's average score
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
+    END IF;
+END $$
+
+DELIMITER ;
